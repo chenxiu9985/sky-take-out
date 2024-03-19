483a7c738956d4f5d98e3cee5e54aef943cf49f1
chenxiu <chenxiu9985@gmail.com>
2024-03-19 20:34:53 +0800

    员工管理

--- sky-pojo/src/main/java/com/sky/entity/Employee.java
+++ sky-pojo/src/main/java/com/sky/entity/Employee.java
@@ -1,5 +1,6 @@
 package com.sky.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -34,10 +35,11 @@ public class Employee implements Serializable {
 
     private Integer status;
 
-    // @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") // 会导致前端接收到的时间格式不对
+    /**
+     * 修改返回前端格式，由LocalDateTime转换为String
+      */
     private LocalDateTime createTime;
 
-    // @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;
 
     private Long createUser;
